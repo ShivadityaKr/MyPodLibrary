3829ac8d313535d5fe22c349a25249075c43094f
shivaditya.thakur <shivaditya.thakur@bigohtech.com>
2022-04-23 22:02:19 +0530

    Initial commit

---
+++ Example/MyPodLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyPodLibrary
+//
+//  Created by ShivadityaKr on 04/23/2022.
+//  Copyright (c) 2022 ShivadityaKr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
